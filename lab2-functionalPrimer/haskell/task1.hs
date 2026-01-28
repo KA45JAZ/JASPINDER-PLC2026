59caaa9a5d21092f14336944d9a42f88a9f9584b
Jaspinder Shergill <145550799+KA45JAZ@users.noreply.github.com>
2026-01-28 10:59:24 +0000

    task 1 complete

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -3,9 +3,9 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (myFunc2 10)
+    print (m3rdFunc)
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc2 y = y*2
+m3rdFunc = "Hello, this is 3rd Func"
